e98c34008c15fa9e09d4702dd3d97c85a51a9a51
Ken Diehl <diehlkj.99@gmail.com>
2020-10-02 23:08:08 -0400

    merge sort complete, test passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -27,5 +27,23 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
+    
+    # # ? find the middle index of arr in given range
+    # lower = 0
+    # upper = 0
+    # middle = int((start + end) / 2)
+    
+    # if start <= end:
+    #     if arr[middle] == target:
+    #         return arr.index(target)
+    #     else:
+    #         if target > arr[middle]:
+    #             # ? search range middle to len(arr) and repeat
+    #             return binary_search(arr, target, middle, end)
+                
+    #         elif target < arr[middle]:
+    #             # ? search range 0 to middle and repeat
+    #             return binary_search(arr, target, start, middle)
+    # else:
+    #     return -1
     pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -11,18 +11,76 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    # ? is array length greater than 1?
+        # * Recursive Part
+        # ? find middle of array
+        # ? left = call merge_sort on arr[:middle] (left side)
+        # ? right = call merge_sort on arr[middle:] (right side)
 
-
+        # * Sorting Part
+        # ? def Left iterate
+        # ? def Right iterate
+        # ? def arr insertion point
+            # * Sort L & R
+        # ? while L < len(left) and R < len(right):
+            # ? check left < right:
+                # ? insert left into arr at insertion point
+                # ? left iterate += 1
+            # ? else:
+                # ? insert right into arr at insertion point
+                # ? right iterate += 1
+            # ? insertion point += 1
+            
+            # * Insert any remaining L & R values bc first while loop only runs until L or R equates to respective length
+            # * These remaining values should already be in order
+        # ? while left iterate less than len of left arr
+            # ? insert left in arr at insertion point
+            # ? left iterate += 1
+            # ? insertion point += 1
+            
+        # ? while right iterate less than len of right arr
+            # ? insert right in arr at insertion point
+            # ? right iterate += 1
+            # ? insertion point += 1
+            
+    if len(arr) > 1:
+        middle = int(len(arr) / 2)
+        left = merge_sort(arr[:middle])
+        right = merge_sort(arr[middle:])
+        
+        L = 0   # ? def Left iterate for returned left arr index
+        R = 0   # ? def Right iterate for returned right arr index
+        i = 0   # ? def arr insertion point
+        
+        while L < len(left) and R < len(right):
+            if left[L] < right[R]:
+                arr[i] = left[L]
+                L += 1
+            else:
+                arr[i] = right[R]
+                R += 1
+            i += 1
+            
+        while L < len(left):
+            arr[i] = left[L]
+            L += 1
+            i += 1
+        while R < len(right):
+            arr[i] = right[R]
+            R += 1
+            i += 1
     return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
+
+# def merge_in_place(arr, start, mid, end):
     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
+
+# def merge_sort_in_place(arr, l, r):
     # Your code here
 
